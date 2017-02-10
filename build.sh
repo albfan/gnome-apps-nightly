b1b5e7c433ce3441141d4f23e6cc2a0c530b3023
Alexander Larsson <alexl@redhat.com>
2017-02-10 16:44:39 +0100

    Use --rebuild-on-sdk-change on master

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ FILE=$1
 
 APPID=`basename $FILE .json`
 
+# Only use this on master branch
+NIGHTLY_ARGS=--rebuild-on-sdk-change
+
 echo ========== Building $APPID ================
-flatpak-builder --force-clean --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${EXPORT_ARGS-} app $FILE
+flatpak-builder --force-clean --ccache --require-changes --repo=repo --subject="Nightly build of ${APPID}, `date`" ${NIGHTLY_ARGS-} ${EXPORT_ARGS-} app $FILE
